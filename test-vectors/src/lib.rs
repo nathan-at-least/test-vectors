289cfec1fb914619103c8bbb26665c7b5484af48
Nate Wilcox <nathan+dev@electriccoin.co>
2022-11-07 07:03:42 -0800

    Initial PoC for `Utf8Str` - prototyped in `macros/tests/basic.rs`.

--- test-vectors/src/lib.rs
+++ test-vectors/src/lib.rs
@@ -1,1 +1,4 @@
+mod utf8str;
+
+pub use self::utf8str::Utf8Str;
 pub use test_vectors_macro::test_vectors;
---
+++ test-vectors/src/utf8str.rs
@@ -0,0 +1,46 @@
+use std::cmp::PartialEq;
+use std::ops::Deref;
+use std::str::Utf8Error;
+
+/// Convenience type for tests with string vectors
+///
+/// A `Utf8Str<'a>` should behave identically to `&'a str` except it implements
+/// `TryFrom<&[u8]>` with UTF-8 decoding.  This provides a convenient short hand for
+/// [macro@test_vectors](crate::test_vectors) tests which operate on strings, rather than bytes.
+///
+/// # Example
+///
+/// ```ignore
+/// #[test_vectors(dir = "tests/basic")]
+/// fn test_replace_spaces(input: Utf8Str<'static>, expected: Utf8Str<'static>) {
+///     let output = input.replace(' ', "_");
+///     assert_eq!(expected, output);
+/// }
+/// ```
+#[derive(Debug)]
+pub struct Utf8Str<'a>(&'a str);
+
+impl<'a> Deref for Utf8Str<'a> {
+    type Target = str;
+
+    fn deref(&self) -> &str {
+        self.0
+    }
+}
+
+impl<'a> TryFrom<&'a [u8]> for Utf8Str<'a> {
+    type Error = Utf8Error;
+
+    fn try_from(bytes: &'a [u8]) -> Result<Utf8Str<'a>, Utf8Error> {
+        std::str::from_utf8(bytes).map(Utf8Str)
+    }
+}
+
+impl<'a, Rhs> PartialEq<Rhs> for Utf8Str<'a>
+where
+    str: PartialEq<Rhs>,
+{
+    fn eq(&self, other: &Rhs) -> bool {
+        self.0.eq(other)
+    }
+}
